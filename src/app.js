b27337c20e8082103331dd896a87b8276d5493df
dan <da.ga.mo.94@gmail.com>
2022-04-21 09:49:46 -0600

    setup app.js, create errors dir with corresponding files inside, add create:migrations script to package.json(test after this commit)

--- src/app.js
+++ src/app.js
@@ -2,4 +2,23 @@ if (process.env.USER) require("dotenv").config();
 const express = require("express");
 const app = express();
 
+const cors = require("cors");
+
+const notFound = require("./errors/notFound");
+const errorHandler = require("./errors/errorHandler");
+
+const moviesRouter = require("./routes/movies/movies.router");
+const reviewsRouter = require("./routes/reviews/reviews.router");
+const theatersRouter = require("./routes/theaters/theaters.router");
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/movies", moviesRouter);
+app.use("/reviews", reviewsRouter);
+app.use("/theatersRouter", theatersRouter);
+
+app.use(notFound);
+app.use(errorHandler);
+
 module.exports = app;
